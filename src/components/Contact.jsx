4e6f7e479f6459e6b6501ceafdf1531906d5305b
Anderson Santos <anderson.chenko1993@gmail.com>
2025-09-17 12:59:39 -0500

    add contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -8,13 +8,75 @@ import facebook from "../assets/facebook.png";
 import twitter from "../assets/twitter.png";
 import whatsapp from "../assets/whatsapp.png";
 
+const icons = [
+  { name: instagram, alt: "Instagram image" },
+  { name: facebook, alt: "Facebook image" },
+  { name: twitter, alt: "Twitter image" },
+  { name: whatsapp, alt: "Whatsapp image" },
+];
+
 export default function Contact() {
   return (
     <section className="contact">
-      <div className="d-flex flex-row justify-content-around">
-        <div className="d-flex">test</div>
-        <div className="d-flex">test</div>
-        <div className="d-flex">test</div>
+      <div className="d-flex justify-content-center gap-5 custom-flex">
+        <div className="d-flex flex-column gap-4">
+          <div className="d-flex">
+            <img
+              className="me-2"
+              src={location}
+              alt="Location image"
+              height="35"
+              width="35"
+            />
+            <span>
+              54421 Tennesse hwy
+              <br />
+              Alexandria, VA, USA, 22303
+            </span>
+          </div>
+          <div className="d-flex">
+            <img
+              className="me-2"
+              src={mail}
+              alt="Mail image"
+              width="30"
+              height="22"
+            />
+            <span>info@palmatennis.com</span>
+          </div>
+          <div className="d-flex">
+            <img className="me-2" src={phone} alt="Phone image" />
+            <span>+34 971 282 000</span>
+          </div>
+        </div>
+        <div className="d-flex flex-column gap-2">
+          <div>
+            <span>Palma de Mallorca</span>
+          </div>
+          <div className="d-flex flex-row gap-5">
+            <div>
+              <span>Scattered Clouds</span>
+              <br />
+              <span>Humidity 73%</span>
+              <br />
+              <span>Wind: 6M/S SW</span>
+              <br />
+              <span>H 21 L 15</span>
+            </div>
+            <div>
+              <span className="fs-1 fw-bolder default-green-600">21º</span>
+            </div>
+          </div>
+        </div>
+        <div className="d-flex flex-row gap-3 gap-md-5">
+          {icons.map((icon, index) => {
+            return (
+              <div key={index}>
+                <img src={icon.name} alt={icon.alt} className="img-fluid" />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </section>
   );
